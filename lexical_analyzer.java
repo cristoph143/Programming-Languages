a157d5245ed4bfe3e4987983ebfd0d19a50ed622
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2022-03-16 08:35:03 +0800

    Update2

--- lexical_analyzer.java
+++ lexical_analyzer.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 /*
     Create a lexical_analyzer that tokenizes the declaration and initialization statements
     of java variables. Store the results in dictionary.
+    The basic form of declaring a variable is:
+        type identifier [= value] [, identifier [= value]]…]; // int x = y = z = 1; or int x, y, z;
+            OR
+        data_type variable_name = value; // int x = 1; or int x;
+        where,
+        type = Data type of the variable // int
+        identifier = Variable name // x
+        value = Data to be stored in the variable (Optional field)
     Here is an example of lexical_analyzer:
     Given source soude: int a = 1;
     Output:
@@ -19,7 +27,11 @@ public class lexical_analyzer {
         try (
                 Scanner scan = new Scanner(System.in);) {
             String lexemes = new String();
-
+            //print the lexemes
+            while (scan.hasNext()) {
+                lexemes = scan.nextLine();
+                System.out.println(lexemes);
+            }
         } catch (Exception e) {
             System.out.println("Error");
         }
