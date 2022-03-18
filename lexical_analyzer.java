666f1515c1161f674a170841df33969149c90bb2
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2022-03-18 16:48:45 +0800

    Update??

--- lexical_analyzer.java
+++ lexical_analyzer.java
@@ -1,4 +1,3 @@
-import java.security.Key;
 import java.util.Scanner;
 
 /*
@@ -61,7 +60,7 @@ public class lexical_analyzer {
     // return the tokenized form of the lexemes by tokenizing the space and ";"
     public static String[] tokenize(String lexemes) {
         String[] tokens = lexemes.split(" ");
-        String[] newTokens = new String[tokens.length + 2];
+        // String[] newTokens = new String[tokens.length + 2];
         //iterate the tokens
         for (int i = 0; i < tokens.length; i++) {
             //if the returned value from getTokenType(tokens[i]) is not null then add
@@ -73,81 +72,38 @@ public class lexical_analyzer {
             //split the tokens[i] by "" and add the it to temp holders array
             else {
                 String[] tempHolders = tokens[i].split("");
-                System.out.println("tempHolders: ");
+                System.out.print("tempHolders: ");
                 //iterate and pr tempHolders
                 for (int j = 0; j < tempHolders.length; j++) {
-                    System.out.println(tempHolders[j]);
+                    System.out.print(tempHolders[j] + " ");
                 }
                 //iterate the tempholders[i] and if the returned value from getTokenType(tempHolders[i]) 
                 //is not null, remove it from the list of tempHolders and add to the id string.
                 String id = "";
                 id = extended_tokenizer(tokens, tempHolders);
-                //while tempHolders is not null, add the index to new TempHolders array
-                String[] tempHolders2 = new String[tempHolders.length];
-                int index = 0;
-                for (String tempHolder : tempHolders) {
-                    if (tempHolder != null) {
-                        tempHolders2[index] = tempHolder;
-                        id = tempHolders2[index];
-                        index++;
-                    }
-                }
-                //copy the tokens to new array and then add the last index of the tempHolders2 array
-                //to the new array.
-                for (int k = 0; k < tokens.length; k++) {
-                    newTokens[k] = tokens[k];
-                    System.out.println("new "+ newTokens[k]);
-                    //print the last token
-                    if (k == tokens.length - 1) {
-                        newTokens[tokens.length -1] = tempHolders2[0];
-                        System.out.println("last token: " + newTokens[k]);
-                    }
-                    System.out.println("news "+ newTokens[k]);
-                }
-                //if the count of tempHolder2 is equal to 1 then add the index value of tempHolders2 to the
-                //last array of tokens
-                // if (tempHolders2.length == 1) {
-                    // tokens[i] += tempHolders2[0];
-                // }
-                //iterate and print the tempHolders
-                for (int j = 0; j < id.length(); j++) {
-                    System.out.println("tmp2 "+ tempHolders2[j]);
-                }
-                // id = extended_tokenizer(tokens,tempHolders2);//iterate and print the tempHolders
-                // for (int j = 0; j < id.length(); j++) {
-                //     System.out.println("tmp2 "+ tempHolders2[j]);
-                // }
-                
-                // System.out.println("uid "+id);
-                // //iterate and print the tempHolders
-                // for (int j = 0; j < tempHolders.length; j++) {
-                //     System.out.println("tmp "+ tempHolders[j]);
-                // }
             }
         }
-        //iterate and print tokens
-        for (int i = 0; i < tokens.length; i++) {
-            System.out.println("stokens "+ tokens[i]);
-        }
-        return newTokens;
+        return tokens;
     }
 
     private static String extended_tokenizer(String[] tokens, String[] tempHolders) {
         String id = "";
-        System.out.println("--id: " + id);
         int count = 0;
+        System.out.println("\n");
         for (int j = 0; j < tempHolders.length; j++) {
-            System.out.println("ou "+tempHolders[j]);
+            System.out.print("before[j]: "+tempHolders[j]);
             if (getTokenType(tempHolders[j]) != null) {
                 id += tempHolders[j];
-                System.out.println("id "+id + " tempHolders[j] "+tempHolders[j]);
+                System.out.print(" id "+id + " tempHolders[j] "+tempHolders[j] + " ");
                 if(getTokenType(id) != null) {
                     //remove value of the tempHolders[j] from the list of tempHolders
                     tempHolders[j] = null;
+                    // System.arraycopy(tempHolders, j + 1, tempHolders, j, tempHolders.length - j - 1);
                     tokens[tokens.length-1] = id;
-                    System.out.println("tok "+tokens[tokens.length-1]);
+                    System.out.print(" After: "+tokens[tokens.length-1] +" Count: "+count +" ");
                     count++;
                 }
+                System.out.println(" Aftered: "+tokens[tokens.length-1] +" Count: "+count);
             }
         }
         System.out.println("count "+count);
@@ -176,7 +132,7 @@ public class lexical_analyzer {
         }
         //else if the token is equal to another "[a-zA-Z_][a-zA-Z0-9_]*" then return "identifier"
         else if (token.matches("[a-zA-Z_][a-zA-Z0-9_]*")) {
-            System.out.println("gettoken: " + token);
+            // System.out.println("gettoken: " + token);
             return "Identifier";
         }
         //else if the token is equal to "[0-9]+" then return "Constant"
